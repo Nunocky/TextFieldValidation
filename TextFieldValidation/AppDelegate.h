042b8b88e33ef3ae20a833fbb555b48eb19e96ff
NUNOKAWA Masato <masato.nunokawa@gmail.com>
2014-05-10 14:43:04 +0900

    Initial Commit

---
+++ TextFieldValidation/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TextFieldValidation
+//
+//  Created by 布川祐人 on 2014/05/10.
+//  Copyright (c) 2014年 NUNOKAWA Masato. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
